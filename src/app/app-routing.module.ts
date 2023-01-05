62413293b94420a43029c759ad9a254a92c6a93e
mugeshp87 <mugeshp087@gmail.com>
2023-01-05 18:02:35 +0530

    push

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+import { ProductsComponent } from './Products/Products.component';
+import { SignupComponent } from './signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{path:'home',component:HomeComponent},
+{path:'products',component:ProductsComponent},
+{path:'login',component:LoginComponent},
+{path:'Signup',component:SignupComponent},
+{path:'',redirectTo:'/home',pathMatch:'full'},
+{path:'**',component:PagenotfoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ProductsComponent } from './Products/Products.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [								
+    AppComponent,HeaderComponent,
+      ProductsComponent,
+      LoginComponent,
+      SignupComponent,
+      FooterComponent,
+      HomeComponent,
+      PagenotfoundComponent
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule
